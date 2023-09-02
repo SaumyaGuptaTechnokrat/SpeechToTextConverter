8bff0a43711be2d48832cea46376ef13a8d419ca
SaumyaGuptaTechnokrat <65768914+SaumyaGuptaTechnokrat@users.noreply.github.com>
2023-09-02 20:47:07 +0530

    Initialising

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
-
+import SpeechRecognition,{useSpeechRecognition} from 'react-speech-recognition';
+import useClipboard from "react-use-clipboard";
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { useState } from 'react';
+import VoiceWaves from './VoiceWaves';
 function App() {
+  const [textToCopy, setTextToCopy] = useState();
+  const [value, setValue]=useState('');
+  const[copied,setCopied]=useState(false, {
+    successDuration:10000
+});
+  const [iscopied, setIsCopied] = useClipboard(textToCopy);
+
+  
+  const { transcript, browserSupportsSpeechRecognition ,listening,stopListening} = useSpeechRecognition();
+  const startListening = ()=>SpeechRecognition.startListening({ continuous: true,language:'en-IN' })
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+  const handleCopied=()=>{
+    setCopied(transcript);
+    console.log(transcript);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <div className='heading'>        <h1>Speech To Text Converter</h1></div>
+      <br/>
+      <div className='main-content' >
+            {transcript}
+      </div>
+      <div >
+        <p style={{display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>Microphone: {listening ? 'on' : 'off'}{listening? <VoiceWaves/> : ''}</p>
+      </div>
+      <div className='btn-style'>
+      <CopyToClipboard text={transcript} onCopy={()=>setCopied}>
+      <button onClick={handleCopied}>
+                        {copied ? 'Copied!' : 'Copy to clipboard'}
+                    </button>
+      </CopyToClipboard>
+     
+                            <button onClick={startListening}>Start Listening</button>
+        <button onClick={SpeechRecognition.stopListening}>Stop Listening</button>
+      
+      </div>
+
+      </div>
   );
 }
 
---
+++ src/VoiceWaves.jsx
@@ -0,0 +1,24 @@
+import React,{ Component } from "react";
+import './App.css';
+class VoiceWaves extends React.Component{
+    render(){
+        return(
+            <>
+                <div className="wave-container">
+                <div className="wave"></div>
+                <div className="wave"></div>
+                <div className="wave"></div>
+                <div className="wave"></div>
+                <div className="wave"></div>
+                <div className="wave"></div>
+                <div className="wave"></div>
+                <div className="wave"></div>
+                <div className="wave"></div>
+                <div className="wave"></div>
+                </div>
+            </>
+        );
+
+    }
+}
+export default VoiceWaves;
